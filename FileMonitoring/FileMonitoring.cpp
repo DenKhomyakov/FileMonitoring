286616c1efaa60a310f96d046b42555f83b73fca
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2024-04-07 17:25:31 +0700

    Updatethe constructor realisation

--- FileMonitoring/FileMonitoring.cpp
+++ FileMonitoring/FileMonitoring.cpp
@@ -4,6 +4,15 @@
 FileMonitoring::FileMonitoring(const QString& filePath) : QFileInfo(filePath) {
     this->filePath = filePath;
     fileInfo = QFileInfo(filePath);
+
+    if (fileInfo.isFile()) {
+        qDebug() << "File name: " << fileInfo.fileName();
+        qDebug() << "File path: " << filePath;
+        qDebug() <<  "File size: " << fileInfo.size();
+        qDebug() << "Date and time of creation: " <<  fileInfo.birthTime();
+    } else {
+        qDebug() << "Error: The file was not found in the specified path";
+    }
 }
 
 QString FileMonitoring::getFilePath() {
